7c5acfab1330a9f89ee0df726ad016b2dd05bb31
asliddin05 <asliddinalimov05@gmail.com>
2023-09-05 05:59:02 +0500

    I finished the tasak

---
+++ main.js
@@ -0,0 +1,32 @@
+// obyekt ichidagi malumotni boshqa obyektga ko'chirish
+
+// let obj = {
+//     id: 1,
+//     name: "Leanne Graham",
+//     username: "Bret",
+//     email: "Sincere@april.biz",
+//     address: {
+//      street: "Kulas Light",
+//       suite: "Apt. 556",
+//       city: "Gwenborough",
+//       zipcode: "92998-3874",
+//       geo: {
+//         lat: "-37.3159",
+//         lng: "81.1496"
+//       }
+//     },
+//     phone: "1-770-736-8031 x56442",
+//     website: "hildegard.org",
+//     company: {
+//       name: "Romaguera-Crona",
+//       catchPhrase: "Multi-layered client-server neural-net",
+//       bs: "harness real-time e-markets"
+//     }
+//   }
+
+// let obj2 ={nestObj:{...obj.address.geo}}
+// let obj2 ={nestObj:{...obj.address.geo.lat}}
+// let obj2 ={nestObj:{...obj.company}}
+// let obj2 ={newObj:{...obj}}
+
+// console.log(obj2);
